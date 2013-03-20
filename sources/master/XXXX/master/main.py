8ddc09de745aad7d5900b11736abe82e83853c5c
Alvaro Saurin <saurin@tid.es>
2013-03-20 22:36:13 +0100

    Prepare for using Circus by having a "master" process and a "app"

---
+++ sources/master/XXXX/master/main.py
@@ -0,0 +1,7 @@
+#
+#
+#
+
+def main():
+    print "hello from the master"
+
--- sources/master/setup.py
+++ sources/master/setup.py
@@ -9,26 +9,24 @@ from setuptools         import setup, find_packages, Extension
 
 __HERE__    = os.path.abspath(os.path.dirname(__file__))
 
-try:    __VERSION__ = open(os.path.join(__HERE__, '..', 'VERSION'), 'r').read().strip()
+try:    __VERSION__ = open(os.path.join(__HERE__, '..', '..', '..', 'VERSION'), 'r').read().strip()
 except: __VERSION__ = os.environ.get('XXXX_VERSION', '0.1')
 
-try:    __RELEASE__ = open(os.path.join(__HERE__, '..', 'RELEASE'), 'r').read().strip()
+try:    __RELEASE__ = open(os.path.join(__HERE__, '..', '..', '..', 'RELEASE'), 'r').read().strip()
 except: __RELEASE__ = os.environ.get('XXXX_RELEASE', '1')
 
 
 # the entry points
 entry_points = """
 [console_scripts]
-XXXX             = XXXX:main
+XXXX-master         = XXXX.master.main:main
 
 """
 
-packages = find_packages(os.path.dirname(__file__))
-
 setup(
-    name                  = 'XXXX',
+    name                  = 'XXXX.master',
     version               = '%s-%s' % (__VERSION__, __RELEASE__),
-    description           = 'The XXXX software distribution',
+    description           = 'The XXXX master process controller',
 
     author                = 'unknown',
 
@@ -38,14 +36,14 @@ setup(
     url                   = 'http://www.company.com',
     license               = 'BSD',
 
-    packages              = packages,
+    packages              = find_packages(__HERE__),
     package_dir           = {
         '' : '.'
     },
 
     zip_safe              = False,
 
-    namespace_packages    = ['XXXX'],       # (so we can install other packages under the 'XXXX' namespace)
+    namespace_packages    = ['XXXX', 'XXXX.master'],       # (so we can install other packages under the 'XXXX' namespace)
 
     entry_points          = entry_points,
 )
